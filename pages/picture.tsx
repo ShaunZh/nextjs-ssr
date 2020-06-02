a5b0dcd3b03aafa69924d36a92f1d2c1107d99b1
ShaunZh <308826842@qq.com>
2020-06-02 19:12:10 +0800

    add: 在客户端调用api接口获取数据，并导出html文件

--- pages/picture.tsx
+++ pages/picture.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React from 'react';
+import fetch from '../libs/fetch';
+import Comment from '../components/Comment';
 
-const Picture: React.FC = () => {
-  return <div className="page-picture">图文</div>;
-};
+export default class ReactStatic extends React.Component {
+  state = {
+    list: [],
+  };
 
-export default Picture;
+  componentDidMount() {
+    console.log('picture componentDidMount');
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const data = await fetch('http://127.0.0.1:7001/temp');
+    console.log('fetch: ', data);
+    this.setState({
+      // @ts-ignore
+      list: data?.list || [],
+    });
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div className="page-react">
+        <div className="content">
+          {list ? <Comment commentsList={list} /> : 'loading'}
+        </div>
+        page - picture
+      </div>
+    );
+  }
+}
