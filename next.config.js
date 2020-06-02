b708c52a7009ced5dacc3e6efe9bc9d4af909beb
ShaunZh <308826842@qq.com>
2020-06-02 19:43:34 +0800

    add: 导出html配置

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
@@ -8,9 +10,14 @@ module.exports = {
     return config;
   },
   async exportPathMap() {
+    // 配置输出的html路径
     return {
       '/': { page: '/' },
+      '/picture': { page: '/picture' },
+      '/live': { page: '/live/[id]' },
+
       // '/home': { page: '/index' },
     };
   },
+  assetPrefix: isProd ? '.' : '',
 };
